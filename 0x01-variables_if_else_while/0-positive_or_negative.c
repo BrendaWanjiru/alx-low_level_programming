9aa9f49d67c2166d28096c2c00b86df63be8fbd6
BrendaWanjiru <brendawanjiru5@gmail.com>
2023-03-20 09:35:49 -0700

    o variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,9 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry poin:t
+ * main - Entry point
  *
- * Return; Always 0 (success)
+ * Return, Always 0 (success)
 */
 
 int main(void)
