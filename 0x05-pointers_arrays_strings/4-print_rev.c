2609265357d19215122eec8a641d2625a85bf971
BrendaWanjiru <brendawanjiru5@gmail.com>
2023-03-28 11:10:11 -0700

    4 print rev c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -9,6 +8,7 @@ void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
+
 	while (*s != '\0')
 	{
 		longi++;
@@ -20,5 +20,6 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
