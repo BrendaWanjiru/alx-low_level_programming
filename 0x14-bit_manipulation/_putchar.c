679be063cf56a8b52538f276bb78bf62c798ec14
BrendaWanjiru <brendawanjiru5@gmail.com>
2023-05-05 00:43:55 -0700

    0 binary to unit c

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints character c to stdout
+ * @c: character to print
+ *
+ * Return: Upon success 1.
+ * On error, -1 returned, and errno set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
