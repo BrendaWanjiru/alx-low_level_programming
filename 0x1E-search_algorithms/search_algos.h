5eec9b47f61e67b5eebae10902bdacb86493922f
BrendaWanjiru <brendawanjiru5@gmail.com>
2023-12-15 19:30:28 +0300

    sorting algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int main(void);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+int binary_search(int *array, size_t size, int value);
+
+#endif
